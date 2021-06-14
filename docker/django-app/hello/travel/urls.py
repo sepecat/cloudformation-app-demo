ce66718b05b914bf7948d3eda7301552410f7ae8
sepecat <ogdrujehad@gmail.com>
2021-06-13 21:55:16 -0500

    remove extra views, add HTML templating and CSS styles

--- docker/django-app/hello/travel/urls.py
+++ docker/django-app/hello/travel/urls.py
@@ -5,12 +5,6 @@ from . import views
 
 app_name = 'travel'
 urlpatterns = [
-    # ex: /polls/
     path('', views.IndexView.as_view(), name='index'),
-    # ex: /polls/5/
     path('<int:countryrestriction_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    # ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- docker/django-app/hello/travel/views.py
+++ docker/django-app/hello/travel/views.py
@@ -1,10 +1,8 @@
 from django.forms import ModelForm
-from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
 from django.views import generic
 
-from .models import Country, CountryRestriction
+from .models import CountryRestriction
 
 
 class IndexView(generic.ListView):
@@ -29,27 +27,3 @@ def detail(request, countryrestriction_id):
         'travel/detail.html',
         {'form': form}
     )
-
-
-class ResultsView(generic.DetailView):
-    model = Country
-    template_name = 'travel/results.html'
-
-
-def vote(request, question_id):
-    question = get_object_or_404(Country, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, CountryRestriction.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(request, 'travel/detail.html', {
-            'question': question,
-            'error_message': "You didn't select a choice.",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('travel:results', args=(question.id,)))
